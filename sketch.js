acc5a5fef0a0cd41fafca165dcf72cff8d16b658
BeatrizBueno04 <00001099401483sp@al.educacao.sp.gov.br>
2024-05-28 11:22:08 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+    creteCanvas(600, 600);
+    background("black");
+  }
+
+  function draw() {
+    stroke("blue");
+    fill("red");
+
+    if(mouseIsPressed){
+       Reflect(mouseX, mouseY, 20, 35)
+    }
+  }
